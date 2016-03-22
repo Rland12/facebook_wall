154385e36c31fcf82094c5c624219548b496d18d
Rland12 <rland7596@withs.org>
2016-03-22 15:33:53 +0000

    Facebook Wall Project

--- js/wall.js
+++ js/wall.js
@@ -5,11 +5,12 @@ $(document).ready(function()
 	$("#post-btn").click(function(){
 
 		var text = $("#message").val();
-
-		 $("#wall").append("<div>" + "<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text  + "</div>");
-
+		
+		 $("#wall").prepend("<div>" +"<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + text );
+	 
 		  $("img").width(90);
-		  
+		   $("img").css("margin-right", 20);
+		 $("img").css("margin-top", 43);
 		  
 	});
 	
